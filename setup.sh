2a6634adf55e73d493fbc626634a8bbe9e62eb59
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-10-22 15:34:14 +0200

    add already installed elm check

--- setup.sh
+++ setup.sh
@@ -4,11 +4,15 @@ set -e
 
 cd "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
-echo "installing elm globally..."
-sudo npm install -g elm
+if ! command -v elm > /dev/null; then
+  echo "installing elm globally..."
+  sudo npm install -g elm
+fi
 echo "installing project dependencies..."
 npm install
+
 echo "installing project elm packages..."
 elm package install --yes
+
 echo "compiling elm files..."
 elm make
