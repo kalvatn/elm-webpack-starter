58d381522bda88b2b046546338954c759b07f24d
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-11-01 19:25:14 +0100

    WIP: markdown api commit

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,50 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 var index = require('./routes/index');
 var users = require('./routes/users');
-var markdown = require('./routes/markdown');
+var markdown = require('./routes/markdown-serve-routes');
+var walk = require('walk');
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('view options', { pretty:true });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/markdown', markdown.handler);
+app.get('/markdown/links', function (req, res) {
+  var validExtensions = [ 'md', 'mdown', 'markdown']
+  var markdownFiles = [];
+  var walker = walk.walk('public/markdown', { followLinks: false });
+  walker.on('file', function(root, stat, next) {
+    var extension = path.extname(stat.name).substring(1);
+    if (validExtensions.indexOf(extension.toLowerCase()) > -1) {
+      markdownFiles.push(root.substring(root.indexOf('/') +1) + '/' + stat.name);
+    } else {
+      console.warn('not a valid markdown file : ' + stat.name);
+    }
+    next();
+  });
+
+  walker.on('end', function() {
+    res.send(markdownFiles);
+  });
+
+});
+app.use('/markdown/edit', markdown.handler);
+
+// app.use('/markdown2', markdown2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
