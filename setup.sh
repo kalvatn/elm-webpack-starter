2943230115f67b044811d796137a628c1d3289a9
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-11-01 11:46:48 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,30 +2,26 @@
 
 set -e
 
-cd "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
 if ! command -v npm > /dev/null; then
-  echo "installing node 6.x ..."
+  echo "installing node 6.x globally ..."
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   sudo apt-get install -y nodejs
 fi
 
 if ! command -v elm > /dev/null; then
-  echo "installing elm ..."
+  echo "installing elm globally ..."
   sudo npm install -g elm
 fi
 
 if ! command -v yarn > /dev/null; then
-  echo "installing yarn ..."
+  echo "installing yarn globally ..."
   sudo npm install -g yarn
 fi
 
-echo "installing project dependencies ..."
-# npm install
-yarn
-
-echo "installing project elm packages ..."
-elm package install --yes
-
-echo "compiling elm files ..."
-elm make
+for dir in backend frontend; do
+  cd "$ROOT/$dir"
+  echo "installing project $dir dependencies ..."
+  yarn
+done
